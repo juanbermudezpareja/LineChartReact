df9af7002427a50227eb7417d4478ac148753943
ebejuan <juan.bermudez.pareja@ericsson.com>
2018-01-27 17:57:26 +0100

    Add favicon webpack

--- webpack.config.js
+++ webpack.config.js
@@ -79,6 +79,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename: 'index.html', // Name of file in ./dist/
       template: 'index.html', // Name of template in ./src
+      favicon: 'favicon.ico',
       hash: true
     }),
     new ExtractTextPlugin({
